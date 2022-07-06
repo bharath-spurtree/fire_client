d1abd226a7bf0b1f333a50f450e1257e39cb38e7
bharathsampangi <bharath.s@spurtreetech.in>
2022-07-06 09:34:33 +0530

    firebase

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+import { getTokenFunc } from './firebase';
 
 function App() {
+
+  const [isTokenFound, setTokenFound] = useState(false);
+  console.log(isTokenFound)
+  
+  getTokenFunc(setTokenFound);
+
   return (
     <div className="App">
       <header className="App-header">
+      {isTokenFound && 
+        <span>Notification permission enabled 👍🏻 </span>
+      }
+      {!isTokenFound && 
+        <span>Need notification permission ❗️</span>
+      }
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
---
+++ src/firebase.js
@@ -0,0 +1,42 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getMessaging, getToken, onMessage } from "firebase/messaging"
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+console.log(process.env.GOOGLE_PROJECT_ID)
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_GOOGLE_API_KEY,
+  authDomain: process.env.REACT_APP_GOOGLE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_GOOGLE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_GOOGLE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_GOOGLE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_GOOGLE_APP_ID,
+  measurementId: process.env.REACT_APP_GOOGLE_MEASUREMENT_ID
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const messaging = getMessaging(app)
+const analytics = getAnalytics(app);
+
+export const getTokenFunc = (setTokenFound) => {
+    return getToken(messaging, {vapidKey: process.env.REACT_APP_GOOGLE_VAPID_KEY}).then((currentToken) => {
+      if (currentToken) {
+        console.log('current token for client: ', currentToken);
+        setTokenFound(true);
+        // Track the token -> client mapping, by sending to backend server
+        // show on the UI that permission is secured
+      } else {
+        console.log('No registration token available. Request permission to generate one.');
+        setTokenFound(false);
+        // shows on the UI that permission is required 
+      }
+    }).catch((err) => {
+      console.log('An error occurred while retrieving token. ', err);
+      // catch error while creating client token
+    });
+}
