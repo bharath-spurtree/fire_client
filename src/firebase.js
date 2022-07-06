a5bad8cf9792002c1d0a47f01fa1100877127f57
bharathsampangi <bharath.s@spurtreetech.in>
2022-07-06 09:52:53 +0530

    firebase

--- src/firebase.js
+++ src/firebase.js
@@ -7,7 +7,7 @@ import { getMessaging, getToken, onMessage } from "firebase/messaging"
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-console.log(process.env.GOOGLE_PROJECT_ID)
+console.log(process.env.REACT_APP_GOOGLE_PROJECT_ID)
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_GOOGLE_API_KEY,
   authDomain: process.env.REACT_APP_GOOGLE_AUTH_DOMAIN,
